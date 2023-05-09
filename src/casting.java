f467e90f21e85e0e71f7b67822595c128a2477b8
Karen Ngo <uminhngo@gmail.com>
2023-05-09 10:27:34 -0400

    Initial commit

---
+++ src/casting.java
@@ -0,0 +1,16 @@
+public class casting {
+    public static void main(String[] args) {
+        double data = 112.35;
+        System.out.println("Double: " + data);
+
+        int num = (int)data;
+        System.out.println("int: " + num);
+
+        String words = "49";
+        System.out.println("The string value is: " + words);
+
+        int numbs = Integer.parseInt(words);
+        System.out.println("The integer is: " + numbs);
+
+    }
+}
